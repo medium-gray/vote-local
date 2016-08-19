71e1b2031a864c3504ddd1df75d0ddeee87f2b11
[olivia d'angelo] <[olivia.dangelo@gmail.com]>
2016-08-19 09:03:48 -0400

    removing storyboard

---
+++ vote-local/vote-local/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  vote-local
+//
+//  Created by White Walls on 8/19/16.
+//  Copyright © 2016 medium-gray. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.blueColor()
+    }
+}
